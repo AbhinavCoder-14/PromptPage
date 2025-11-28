01a882f5fd3249db2be84d24378624b6f5d089b2
= <kanikayadav061@gmail.com>
2025-11-28 18:26:02 +0530

    feat:backend is working perfectly fine and all the things emabding vector store and retrival is working fine

--- server/index.js
+++ server/index.js
@@ -3,15 +3,28 @@ import cors from "cors";
 import multer from "multer";
 import { Queue } from "bullmq";
 
+import { GoogleGenAI } from "@google/genai";
+
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { Document, SystemMessage } from "langchain";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { CharacterTextSplitter } from "@langchain/textsplitters";
+
+import "dotenv/config";
+
 const app = express();
 
+const client = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY2,
+});
 
-const queue = new Queue("file-upload",{
-  connection:{
-    host:"localhost",
-    port:6379,
-    password:"ITSMEBBy"
-  }
+const queue = new Queue("file-upload", {
+  connection: {
+    host: "localhost",
+    port: 6379,
+    password: "ITSMEBBy",
+  },
 });
 
 app.use(cors());
@@ -32,17 +45,58 @@ app.get("/", (req, res) => {
   return res.json({ status: "all good bby" });
 });
 
-app.post("/upload/pdf", upload.single("pdf"), async(req, res) => {
+app.get("/chat", async (req, res) => {
+  const query = "what is python?";
 
-  await queue.add("file-ready",{
-    filename:req.file.originalname,
-    destination:req.file.destination,
-    path:req.file.path
-  })
+  const embedding = new GoogleGenerativeAIEmbeddings({
+    model: "text-embedding-004",
+    apiKey: process.env.GEMINI_API_KEY2,
+  });
 
+  const vectorStore = await QdrantVectorStore.fromExistingCollection(
+    embedding,
+    {
+      url: "http://localhost:6333",
+      collectionName: "pdf-docs",
+    }
+  );
 
+  const ret = vectorStore.asRetriever({
+    k: 200,
+  });
 
-  
+  const result = await ret.invoke(query);
+  const SYSTEM_PROMPT = `you are helpfull ai assistant who answer the user query based on the avaliable context form the given pdf file. context: ${JSON.stringify(
+    result
+  )}`;
+
+  const response = await client.models.generateContent({
+    model: "gemini-2.5-flash", // 'gemini-3' does not exist yet; use 1.5-flash or 1.5-pro
+    config: {
+      // System instructions are best placed here in the new SDK
+      systemInstruction: {
+        parts: [{ text: SYSTEM_PROMPT }],
+      },
+    },
+    contents: [
+      {
+        role: "user",
+        parts: [{ text: query }], // Must use 'parts' array with 'text' key
+      },
+    ],
+  });
+
+  res.json({ message: response.text,
+    docs:result
+   });
+});
+
+app.post("/upload/pdf", upload.single("pdf"), async (req, res) => {
+  await queue.add("file-ready", {
+    filename: req.file.originalname,
+    destination: req.file.destination,
+    path: req.file.path,
+  });
 
   console.log("upload the given pdf file :)");
 });
--- server/worker.js
+++ server/worker.js
@@ -7,52 +7,54 @@ import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { CharacterTextSplitter } from "@langchain/textsplitters";
 import "dotenv/config";
 
-import PDFParse from "pdf-parse";
+// import PDFParse from "pdf-parse";
 
-import { QdrantClient } from "@qdrant/js-client-rest";
-import { Embeddings } from "@langchain/core/embeddings";
+// import { QdrantClient } from "@qdrant/js-client-rest";
+// import { Embeddings } from "@langchain/core/embeddings";
 
-// console.log("enterd in worder")
+// console.log("enterd in worker")
 
 const worker = new Worker(
   "file-upload",
   async (job) => {
-    console.log("enterd in worder");
+    console.log("enterd in worker");
 
     try {
       const loader = new PDFLoader(job.data.path);
       const docs = await loader.load();
       console.log(docs);
 
-      const textsplitters = new CharacterTextSplitter({
-        chunkSize: 300,
-        chunkOverlap: 0,
-      });
+      // const textsplitters = new CharacterTextSplitter({
+      //   chunkSize: 300,
+      //   chunkOverlap: 0,
+      // });
 
-      const texts = await textsplitters.splitDocuments(docs);
-      console.log(texts);
-      console.log("Job:", job.data);
+      // const texts = await textsplitters.splitDocuments(docs);
+      // console.log(texts);
+      // console.log("Job:", job.data);
 
       const embedding = new GoogleGenerativeAIEmbeddings({
         model: "text-embedding-004",
         apiKey: process.env.GEMINI_API_KEY,
       });
 
-
-      const vectorStore = await QdrantVectorStore.fromExistingCollection(embedding,{
-        url:"http://localhost:6333",
-        collectionName:"pdf-docs"
-      })
+      const vectorStore = await QdrantVectorStore.fromExistingCollection(
+        embedding,
+        {
+          url: "http://localhost:6333",
+          collectionName: "pdf-docs",
+        }
+      );
 
       await vectorStore.addDocuments(docs);
 
-      console.log("All docs data is added in the vector database")
+      console.log("All docs data is added in the vector database");
     } catch (err) {
       console.log(err);
     }
   },
   {
-    concurrency: 5,
+    concurrency: 100,
     connection: {
       host: "localhost",
       port: 6379,
